e9402ae051ec1db871c68059426504408ed5d71c
gyanish77 <gyanish@luminoguru.com>
2022-05-18 17:23:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Box } from "@material-ui/core";
+import "./App.css";
+import GitHub from "./components/GitHub";
+
+function App() {
+  return (
+    <Box className="App">
+      <GitHub />
+    </Box>
+  );
+}
+
+export default App;
